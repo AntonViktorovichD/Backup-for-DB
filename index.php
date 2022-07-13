ab2b8ba25eba19267f03ef3851555a26eba77eae
AntonViktorovichD <white.trash.without.cash@gmail.com>
2022-07-13 13:43:15 +0300

    Start making

--- index.php
+++ index.php
@@ -6,47 +6,67 @@ try {
    echo 'Выброшено исключение: ', $e->getMessage(), "\n";
 }
 
+date_default_timezone_set('Europe/Moscow');
+
 $tables = $dbh->query('SHOW TABLES')->fetchAll();
 
-$sql_arr = [];
+$sql_cols_arr = [];
 
 //var_dump($dbh->query("SHOW FIELDS FROM tables")->fetchAll());
 
 foreach ($tables as $key => $table) {
 
    $stat = $dbh->query("SHOW TABLE STATUS FROM `laravel` WHERE `name` LIKE '" . $table[0] . "' ")->fetchAll();
-//   var_dump($stat);
-   $sql_arr[$table[0]]['status']['Engine'] = $stat[0]['Engine'];
-   $sql_arr[$table[0]]['status']['Auto_increment'] = $stat[0]['Auto_increment'];
-   $sql_arr[$table[0]]['status']['Collation'] = $stat[0]['Collation'];
+   $sql_cols_arr[$table[0]]['status']['Engine'] = $stat[0]['Engine'];
+   $sql_cols_arr[$table[0]]['status']['Auto_increment'] = $stat[0]['Auto_increment'];
+   $sql_cols_arr[$table[0]]['status']['Collation'] = $stat[0]['Collation'];
 
    $cols = $dbh->query("SHOW FIELDS FROM " . $table[0] . "")->fetchAll();
 
-   foreach ($cols as $key => $col) {
-      var_dump($col);
-      $sql_arr[$table[0]]['cols'][$key]['Field'] = $col['Field'];
-      $sql_arr[$table[0]]['cols'][$key]['Type'] = $col['Type'];
+   foreach ($cols as $k => $col) {
+      $sql_cols_arr[$table[0]]['cols'][$key]['Field'] = $col['Field'];
+      $sql_cols_arr[$table[0]]['cols'][$key]['Type'] = $col['Type'];
       if ($col['Null'] == 'NO') {
-         $sql_arr[$table[0]]['cols'][$key]['Null'] = 'NOT NULL';
+         $sql_cols_arr[$table[0]]['cols'][$key]['Null'] = 'NOT NULL';
       } else {
-         $sql_arr[$table[0]]['cols'][$key]['Null'] = 'DEFAULT NULL';
+         $sql_cols_arr[$table[0]]['cols'][$key]['Null'] = 'DEFAULT NULL';
       }
 
       if (strlen($col['Key']) > 0) {
-         $sql_arr[$table[0]]['PK'] = "PRIMARY KEY (`" . $col['Field'] . "`)";
+         $sql_cols_arr[$table[0]]['PK'] = "PRIMARY KEY (`" . $col['Field'] . "`)";
       }
 
       if (strlen($col['Extra']) > 0) {
-         $sql_arr[$table[0]]['cols'][$key]['Extra'] = "AUTO_INCREMENT";
+         $sql_cols_arr[$table[0]]['cols'][$key]['Extra'] = "AUTO_INCREMENT";
       }
-
    }
+//   var_dump($sql_table);
 
-//
-//   $sql_arr[$table[0]]['cols']['Type'] = $cols[$key]['Type'];
-
-//   var_dump($dbh->query('SELECT * FROM ' . $table[0])->fetchAll());
+//   $fp = fopen($filename . ".sql", "w");
+//   fwrite($fp, $sql_table);
+//   fclose($fp);
 }
 
-//var_dump($sql_arr);
-var_dump($sql_arr['daily_reports']);
+$filename = date("d_m_y") . '_' . $table[0];
+
+foreach ($sql_cols_arr as $key => $val) {
+
+}
+$sql_table = 'CREATE TABLE IF NOT EXISTS `' . $table[0] . '` (
+  `' . $col['Field'] . '` smallint(6) NOT NULL AUTO_INCREMENT,
+  `' . $col['Field'] . '` varchar(256) NOT NULL,
+  `' . $col['Field'] . '` varchar(128) NOT NULL,
+  `' . $col['Field'] . '` varchar(128) NOT NULL,
+  `' . $col['Field'] . '` smallint(6) NOT NULL,
+  `' . $col['Field'] . '` smallint(6) NOT NULL,
+  `' . $col['Field'] . '` json DEFAULT NULL,
+  `' . $col['Field'] . '` timestamp NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=196 DEFAULT CHARSET=utf8mb4;';
+var_dump($sql_cols_arr);
+//var_dump($sql_cols_arr['daily_reports']);
+
+//
+//   $sql_cols_arr[$table[0]]['cols']['Type'] = $cols[$key]['Type'];
+
+//   var_dump($dbh->query('SELECT * FROM ' . $table[0])->fetchAll());
