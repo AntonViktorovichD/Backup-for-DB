75524c3bad8bcfdb872ef11c916fb79fa4ca7844
AntonViktorovichD <white.trash.without.cash@gmail.com>
2022-07-13 14:59:05 +0300

    Start making

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+
 try {
    $dbh = new PDO('mysql:host=localhost; dbname=laravel; charset=utf8mb4', 'root', '', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
    $data = $dbh->query("SELECT * FROM tables")->fetchAll();
@@ -14,20 +15,30 @@ $sql_table = '';
 
 //var_dump($dbh->query("SHOW FIELDS FROM tables")->fetchAll());
 
-foreach ($tables as $key => $table) {
+foreach ($tables as $table) {
 
    $stat = $dbh->query("SHOW TABLE STATUS FROM `laravel` WHERE `name` LIKE '" . $table[0] . "' ")->fetchAll();
 
-   $sql_table = 'CREATE TABLE IF NOT EXISTS `' . $table[0] . '` (
-  `id` smallint(6) NOT NULL AUTO_INCREMENT,
-  `table_name` varchar(256) NOT NULL,
-  `table_uuid` varchar(128) NOT NULL,
-  `row_uuid` varchar(128) NOT NULL,
-  `user_id` smallint(6) NOT NULL,
-  `user_dep` smallint(6) NOT NULL,
-  `json_val` json DEFAULT NULL,
-  `created_at` timestamp NOT NULL,
-  PRIMARY KEY (`id`)
+   $cols = $dbh->query("SHOW FIELDS FROM " . $table[0] . "")->fetchAll();
+   $sql_table = 'CREATE TABLE IF NOT EXISTS `' . $table[0] . '` (' . PHP_EOL;
+
+   foreach ($cols as $key => $col) {
+      var_dump($col);
+      if ($col['Null'] == 'NO') {
+         $null = 'NOT NULL';
+      } else {
+         $null = 'DEFAULT NULL';
+      }
+
+      if ($col['Key'] == 'PRI') {
+         $pri = 'AUTO_INCREMENT,';
+      } else {
+         $pri = ',';
+      }
+      $sql_table .= ('`' . $col['Field'] . '` ' . $col['Type'] . ' ' . $null . $pri) . PHP_EOL;
+
+   }
+   $sql_table .= 'PRIMARY KEY (`id`)
 ) ENGINE=' . $stat[0]['Engine'] . ' AUTO_INCREMENT=' . $stat[0]['Auto_increment'] . ' DEFAULT CHARSET=utf8mb4;';
 
    var_dump($sql_table);
