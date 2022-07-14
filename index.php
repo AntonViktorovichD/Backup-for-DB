0b36eecb8847c6191a4f479a40db5818970c2815
AntonViktorovichD <white.trash.without.cash@gmail.com>
2022-07-14 12:49:27 +0300

    Start making

--- index.php
+++ index.php
@@ -15,19 +15,17 @@ $sql_table = '';
 
 $indx_key = '';
 
-//var_dump($dbh->query("SHOW FIELDS FROM tables")->fetchAll());
+$col_name = '';
+
+$table_val = '';
 
 foreach ($tables as $table) {
 
-
    $stat = $dbh->query("SHOW TABLE STATUS FROM `laravel` WHERE `name` LIKE '" . $table[0] . "' ")->fetchAll();
-
    $cols = $dbh->query("SHOW FIELDS FROM " . $table[0] . "")->fetchAll();
    $sql_table = 'CREATE TABLE IF NOT EXISTS `' . $table[0] . '` (' . PHP_EOL;
 
    foreach ($cols as $key => $col) {
-
-//      var_dump($col);
 
       if ($col['Null'] == 'NO') {
          $null = 'NOT NULL ';
@@ -49,6 +47,11 @@ foreach ($tables as $table) {
          $indx_key .= '';
       }
 
+      if ($key < count($cols) - 1) {
+         $col_name .= '`' . $col['Field'] . '`, ';
+      } else {
+         $col_name .= '`' . $col['Field'] . '`';
+      }
 
 
       $sql_table .= ('`' . $col['Field'] . '` ' . $col['Type'] . ' ' . $mul . ' ' . $null . $pri) . PHP_EOL;
@@ -60,13 +63,38 @@ foreach ($tables as $table) {
       $collate = '';
    }
 
-   $sql_table .= 'PRIMARY KEY (`id`), ' . $indx_key . ' 
-) ENGINE=' . $stat[0]['Engine'] . ' AUTO_INCREMENT=' . $stat[0]['Auto_increment'] . ' DEFAULT CHARSET=utf8mb4 ' . $collate . ';';
+   $sql_table .= 'PRIMARY KEY (`id`), ' . $indx_key . '
+) ENGINE=' . $stat[0]['Engine'] . ' AUTO_INCREMENT=' . $stat[0]['Auto_increment'] . ' DEFAULT CHARSET=utf8mb4 ' . $collate . ';' . PHP_EOL;
    $indx_key = '';
+
+   $values = $dbh->query("SELECT * FROM " . $table[0] . "")->fetchAll();
+
+   foreach ($values as $value) {
+      foreach ($value as $key => $val) {
+         if (is_numeric($key)) {
+            $table_val .= "'" . $val . "', ";
+         }
+         if ($key == 0) {
+            $table_val .= "( " . $table_val;
+         }
+         if ($key == (count($value) / 2) - 1) {
+            $table_val .= $table_val . "), ";
+         }
+      }
+
+   }
+
+
+   $sql_table .= 'INSERT INTO `' . $table[0] . '` (' . $col_name . ') VALUES (';
 
    echo '<pre>';
    print_r($sql_table);
    echo '</pre>';
+
+
+   $col_name = '';
+
+   $table_val = '';
 }
 
 
